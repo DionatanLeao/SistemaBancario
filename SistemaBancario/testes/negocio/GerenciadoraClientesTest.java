6f25dbe2669a388d48c499ed96792ebd8dff8ebb
DionatanLeao <dionatan.ti@outlook.com>
2021-10-31 17:59:14 -0300

    Coding Tests With Programming Techniques

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest.java
@@ -16,8 +16,10 @@ public class GerenciadoraClientesTest {
 	public void testPesquisaCliente() {
 		
 		//Montando cenário
-		Cliente c1 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
-		Cliente c2 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+		int idCliente1 = 1;
+		int idCliente2 = 2;
+		Cliente c1 = new Cliente(idCliente1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
+		Cliente c2 = new Cliente(idCliente2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
 		
 		List<Cliente> list = new ArrayList<>();
 		list.addAll(Arrays.asList(c1, c2));
@@ -25,10 +27,10 @@ public class GerenciadoraClientesTest {
 		GerenciadoraClientes gc = new GerenciadoraClientes(list);
 		
 		//Execução
-		Cliente c3 = gc.pesquisaCliente(1);
+		Cliente c3 = gc.pesquisaCliente(idCliente1);
 		
 		//Verificações
-		assertThat(c3.getId(), is(1));
+		assertThat(c3.getId(), is(idCliente1));
 		assertThat(c3.getEmail(), is("gugafarias@gmail.com"));
 			
 	}
@@ -37,6 +39,8 @@ public class GerenciadoraClientesTest {
 	public void testRemoveCliente() {
 		
 		//Montagem cenário
+		int idCliente1 = 1;
+		int idCliente2 = 2;
 		Cliente c1 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente c2 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
 		
@@ -46,12 +50,12 @@ public class GerenciadoraClientesTest {
 		GerenciadoraClientes gc = new GerenciadoraClientes(list);
 		
 		//Execução
-		boolean clienteRemovido = gc.removeCliente(2);
+		boolean clienteRemovido = gc.removeCliente(idCliente2);
 		
 		//Verificações
 		assertThat(clienteRemovido, is(true));
-		assertThat(gc.getClientesDoBanco().size(), is(1));
-		assertNull(gc.pesquisaCliente(2));
+		assertThat(gc.getClientesDoBanco().size(), is(idCliente1));
+		assertNull(gc.pesquisaCliente(idCliente2));
 	}
 
 }
