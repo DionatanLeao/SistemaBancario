dd3c0f0ed2d0576cbc951895dff67b30d8fb86fd
DionatanLeao <dionatan.ti@outlook.com>
2021-10-31 22:43:15 -0300

    How to Save Time and Optimize Testing with Similar Scenarios

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest.java
@@ -8,23 +8,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GerenciadoraClientesTest {
-
-	@Test
-	public void testPesquisaCliente() {
-		
+	
+	private GerenciadoraClientes gc;
+	private int idCliente1 = 1;
+	private int idCliente2 = 2;
+	
+	@Before
+	public void setUp() {
 		//Montando cenário
-		int idCliente1 = 1;
-		int idCliente2 = 2;
 		Cliente c1 = new Cliente(idCliente1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente c2 = new Cliente(idCliente2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
 		
 		List<Cliente> list = new ArrayList<>();
 		list.addAll(Arrays.asList(c1, c2));
 		
-		GerenciadoraClientes gc = new GerenciadoraClientes(list);
+		gc = new GerenciadoraClientes(list);		
+	}
+	
+	@After
+	public void tearDown() {
+		gc.limpa();
+	}
+
+
+	@Test
+	public void testPesquisaCliente() {		
 		
 		//Execução
 		Cliente c3 = gc.pesquisaCliente(idCliente1);
@@ -37,18 +50,7 @@ public class GerenciadoraClientesTest {
 	
 	@Test
 	public void testRemoveCliente() {
-		
-		//Montagem cenário
-		int idCliente1 = 1;
-		int idCliente2 = 2;
-		Cliente c1 = new Cliente(idCliente1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
-		Cliente c2 = new Cliente(idCliente2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
-		
-		List<Cliente> list = new ArrayList<>();
-		list.addAll(Arrays.asList(c1, c2));
-		
-		GerenciadoraClientes gc = new GerenciadoraClientes(list);
-		
+
 		//Execução
 		boolean clienteRemovido = gc.removeCliente(idCliente2);
 		
